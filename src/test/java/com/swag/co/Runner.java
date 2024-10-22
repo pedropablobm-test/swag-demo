7a1e5f40819dc5d671551b41288d6fc27af33b6f
pedropablobm <pedro.bermudez@correounivalle.edu.co>
2024-10-21 19:35:02 -0500

    Probando con Edge

--- src/test/java/com/swag/co/Runner.java
+++ src/test/java/com/swag/co/Runner.java
@@ -1,4 +1,15 @@
 package com.swag.co;
 
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.swag.co.definitions",
+        tags = "@Login"
+)
+
 public class Runner {
 }
